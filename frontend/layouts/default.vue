09118bc522b664a13151c9f78db02fece61de3b6
Sebastian Verweyen <subjacked@live.de>
2022-05-23 15:00:00 +0200

    [TASK] Added Logo

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="bg-black h-screen flex flex-col page">
     <div class="flex-1 overflow-y-auto">
+      <Logo class="text-white/30 h-12 m-5" />
       <slot />
     </div>
   </div>
 </template>
+<script lang="ts" setup>
+import Logo from '@/assets/images/logo.svg'
+</script>
