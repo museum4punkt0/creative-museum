6caa8db6696dce8d57b5779aa233d3d120c541f9
Sebastian Verweyen <subjacked@live.de>
2022-06-02 09:00:19 +0200

    [TASK] Auth store

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -1,17 +1,12 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-
-Vue.use(Vuex)
-
-const state = {
+export const state = () => ({
   authenticated: false
-}
+})
 
-const getters = {
+export const getters = () => ({
   isAuth: state => state.authenticated
-}
+})
 
-const mutations = {
+export const mutations = () => ({
   login (state) {
     console.log('authenticated!')
     Vue.set(state, 'authenticated', true)
@@ -19,9 +14,9 @@ const mutations = {
   logout (state) {
     Vue.set(state, 'authenticated', false)
   }
-}
+})
 
-const actions = {
+export const actions = () => ({
   nuxtServerInit({ commit }, { req }) {
     if (req.session && req.session.authUser) {
       commit('login')
@@ -36,11 +31,4 @@ const actions = {
   logout ({ commit }) {
     commit('logout')
   }
-}
-
-export default () => new Vuex.Store({
-  state,
-  getters,
-  mutations,
-  actions
 })
