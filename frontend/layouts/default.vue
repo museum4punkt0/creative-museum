97b5ff31a41ee82f22d81a23149f4b3ead970dc7
Sebastian Verweyen <subjacked@live.de>
2022-06-07 16:15:34 +0200

    [TASK] Tutorial

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -14,3 +14,10 @@
     </div>
   </div>
 </template>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'LayoutDefault'
+})
+</script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -13,6 +13,8 @@ export default defineComponent({
 
     if (user.value.tutorial === false) {
       router.push('/tutorial')
+    } else {
+      router.push('/campaigns')
     }
 
   }
