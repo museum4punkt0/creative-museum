0a4e18590c9de35b04c8fd7dfd8115acb2b0b6b8
nicoborschke <nico.borschke@jwied.de>
2022-06-14 14:53:23 +0200

    [TASK] Show only Posts without parent

---
+++ backend/src/DataProvider/PostCollectionProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataProvider;
+
+
+use ApiPlatform\Core\DataProvider\ContextAwareCollectionDataProviderInterface;
+use App\Entity\Post;
+use App\Repository\PostRepository;
+
+final class PostCollectionProvider implements ContextAwareCollectionDataProviderInterface
+{
+    private PostRepository $postRepository;
+
+    public function __construct(PostRepository $postRepository)
+    {
+        $this->postRepository = $postRepository;
+    }
+
+    public function supports(string $resourceClass, string $operationName = null, array $context = []): bool
+    {
+        return Post::class === $resourceClass;
+    }
+
+    public function getCollection(string $resourceClass, string $operationName = null, array $context = []): iterable
+    {
+        return $this->postRepository->findBy([
+            'parent' => null
+        ]);
+    }
+}
