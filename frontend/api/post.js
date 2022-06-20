dc2822f536cbc0317eb29720afac7d5658fefb47
Sebastian Verweyen <subjacked@live.de>
2022-06-20 10:58:36 +0200

    [TASK] Get Posts by campaign

---
+++ frontend/api/post.js
@@ -0,0 +1,20 @@
+import { useContext } from '@nuxtjs/composition-api'
+
+export const postApi = () => {
+  const { $api } = useContext()
+
+  const fetchPost = async (postId) => {
+    const res = await $api.get(`posts/${postId}`)
+    return res
+  }
+
+  const fetchPostsByCampaign = async (campaignId) => {
+    const res = await $api.get(`posts/?campaign=${campaignId}`)
+    return res
+  }
+
+  return {
+    fetchPost,
+    fetchPostsByCampaign,
+  }
+}
