5d5b3e5ecd6fc63d4a208cb75f98ef4e70a40d9b
Sebastian Verweyen <subjacked@live.de>
2022-06-22 12:19:34 +0200

    [TASK] Campaign

--- frontend/components/campaign/Head.vue
+++ frontend/components/campaign/Head.vue
@@ -3,17 +3,12 @@
     <h1 class="page-header" w:m="t-0 b-1">{{ campaign.title }}</h1>
     <div><span w:text="capitalize">{{ $t('till')}}</span> {{ $dayjs(campaign.end).format('DD.MM.YYYY') }}</div>
     <div>{{campaign.shortDescription}}</div>
-    <div><Nl2br tag="p" :text="campaign.description" /></div>
   </div>
 </template>
 <script>
 import { defineComponent } from '@nuxtjs/composition-api'
-import Nl2br from 'vue-nl2br'
 
 export default defineComponent({
-  components: {
-    Nl2br
-  },
   props: {
     campaign: {
       type: Object,
--- frontend/plugins/vue-nl2br.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import Nl2br from 'vue-nl2br'
-
-Vue.component('Nl2br', Nl2br)
