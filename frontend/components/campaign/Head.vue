2f8f592580d6d4a9fa6646702e47e29657557dff
Sebastian Verweyen <subjacked@live.de>
2022-06-28 09:17:54 +0200

    [TASK] Campaign

--- frontend/components/campaign/Head.vue
+++ frontend/components/campaign/Head.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <h1 class="page-header" w:m="t-0 b-1">{{ campaign.title }}</h1>
-    <div><span w:text="capitalize">{{ $t('till')}}</span> {{ $dayjs(campaign.end).format('DD.MM.YYYY') }}</div>
+    <div w:m="b-6">
+      <h1 class="page-header" w:m="t-0 b-1">{{ campaign.title }}</h1>
+      <div w:text="lg"><span w:text="capitalize">{{ $t('till')}}</span> {{ $dayjs(campaign.end).format('DD.MM.YYYY') }}</div>
+    </div>
     <div>{{campaign.shortDescription}}</div>
   </div>
 </template>
