19b5c4d4d2a7405ee6d6d65fbdbe35c1087acdea
Sebastian Verweyen <subjacked@live.de>
2022-07-06 14:16:52 +0200

    [TASK] Voting

--- backend/src/Controller/PostVoteController.php
+++ backend/src/Controller/PostVoteController.php
@@ -90,14 +90,10 @@ class PostVoteController extends AbstractController
         $voteEvent = new NewPostVoteEvent($data->getPost()->getId(), $data->getDirection()->value, $voteDifference, $switched);
         $this->eventDispatcher->dispatch($voteEvent, NewPostVoteEvent::NAME);
 
-        if(is_array($dbVote)){
-            $result = [
-                'vote' => $dbVote,
-                'post' => $this->postRepository->find($data->getPost())
-            ];
-        }else{
-            $result = $dbVote;
-        }
+        $result = [
+            'vote' => $dbVote,
+            'votestotal' => $this->postRepository->find($data->getPost())->getVotestotal()
+        ];
 
         return $result;
     }
