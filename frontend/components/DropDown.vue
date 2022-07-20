a708f616338236e9ced3d6d700c90379fba79fa7
Sebastian Verweyen <subjacked@live.de>
2022-07-20 11:05:16 +0200

    [TASK] Filter

--- frontend/components/DropDown.vue
+++ frontend/components/DropDown.vue
@@ -1,19 +1,21 @@
 <template>
   <div>
-    <button w:px="2" w:pr="6" w:py="1" w:mb="3" w:rounded="full" w:abuttongn="self-start" w:text="sm" w:border="1 white" class="btn-outline btn-dropdown" :class="showOptions ? 'active' : ''" @click.prevent="showOptions = !showOptions">
+    <button w:px="2" w:pr="6" w:py="1" w:mb="3" w:rounded="full" w:align="self-start" w:text="sm" w:border="1 white" class="btn-outline btn-dropdown" :class="showOptions ? 'active' : ''" @click.prevent="toggleDropdown">
       {{ label }}
     </button>
-    <div v-show="showOptions" w:ml="2">
+    <div v-show="showOptions" w:p="x-6 lg:l-0" w:w="screen lg:auto" w:ml="lg:2" w:flex="~ row lg:col" w:pos="absolute lg:static" w:left="0 lg:auto" w:top="8 lg:auto">
       <button
         v-for="(item, key) in options"
         :key="key"
         w:px="2"
         w:py="1"
         w:mb="3"
+        :w:ml="key > 0 ? '3 lg:0' : ''"
         w:rounded="full"
-        w:text="sm"
+        w:text="sm space-nowrap"
         w:border="1 white"
-        class="btn-outline"
+        w:align="self-start"
+        class="btn-outline border-dashed"
         :class="key === selectedValue ? 'active' : ''"
         @click="selectValue(key)"
       >
@@ -37,11 +39,17 @@ export default defineComponent({
     }
   },
   emits: [
-    'dropdownChange'
+    'dropdownChange',
+    'dropdownState'
   ],
   setup(props, context) {
     const showOptions = ref(false)
     const selectedValue = ref(-1)
+
+    function toggleDropdown() {
+      showOptions.value = ! showOptions.value
+      context.emit('dropdownState', showOptions.value)
+    }
 
     function selectValue(value) {
       if (selectedValue.value === value) {
@@ -55,6 +63,7 @@ export default defineComponent({
     return {
       showOptions,
       selectedValue,
+      toggleDropdown,
       selectValue
     }
   },
--- frontend/components/campaign/Filter.vue
+++ frontend/components/campaign/Filter.vue
@@ -1,24 +1,24 @@
 <template>
   <div>
-    <div w:flex="~ col">
+    <div w:pos="relative" w:m="-x-6 lg:0" w:p="x-6 lg:0" :w:h="dropdownHeight ? '20 lg:auto' : 'auto'" w:flex="~ row nowrap md:col" w:snap="x" w:overflow="x-auto" class="snap snap-mandatory snap-x scrollbar-hide">
       <button w:px="2" w:py="1" w:mb="3" w:rounded="full" w:align="self-start" w:text="sm" w:border="1 white" class="btn-outline">
         Neuste
       </button>
-      <button w:px="2" w:py="1" w:mb="3" w:rounded="full" w:align="self-start" w:text="sm" w:border="1 white" class="btn-outline">
+      <button w:px="2" w:py="1" w:ml="3 lg:0" w:mb="3" w:rounded="full" w:align="self-start" w:text="sm" w:border="1 white" class="btn-outline">
         Relevant
       </button>
-      <button w:px="2" w:py="1" w:mb="3" w:rounded="full" w:align="self-start" w:text="sm" w:border="1 white" class="btn-outline">
+      <button w:px="2" w:py="1" w:ml="3 lg:0" w:mb="3" w:rounded="full" w:align="self-start" w:text="sm" w:border="1 white" class="btn-outline">
         Kontrovers
       </button>
-      <DropDown :options="['Wie krass!', 'Super informativ!', 'Du erhellst meinen Tag!', 'Hab ich noch nie gehört.', 'Wie traurig.']" label="Feedback" />
-      <button w:px="2" w:py="1" w:mb="3" w:rounded="full" w:align="self-start" w:text="sm" w:border="1 white" class="btn-outline">
+      <DropDown w:ml="3 lg:0" :options="['Wie krass!', 'Super informativ!', 'Du erhellst meinen Tag!', 'Hab ich noch nie gehört.', 'Wie traurig.']" label="Feedback" @dropdownState="setHeight" />
+      <button w:px="2" w:py="1" w:ml="3 lg:0" w:mb="3" w:rounded="full" w:align="self-start" w:text="sm" w:border="1 white" class="btn-outline">
         Playlist
       </button>
     </div>
   </div>
 </template>
 <script>
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, ref} from '@nuxtjs/composition-api'
 
 export default defineComponent({
   props: {
@@ -28,7 +28,18 @@ export default defineComponent({
     }
   },
   setup() {
-
+    const dropdownHeight = ref(false)
+    function setHeight(params) {
+      if (params === true)  {
+        dropdownHeight.value = true
+      } else {
+        dropdownHeight.value = false
+      }
+    }
+    return {
+      dropdownHeight,
+      setHeight
+    }
   },
 })
 </script>
