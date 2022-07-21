42bcf2427cad73cf1e80cde30fc336b3309efd11
Sebastian Verweyen <subjacked@live.de>
2022-07-21 09:19:22 +0200

    [TASK] Styling

--- frontend/components/post/Head.vue
+++ frontend/components/post/Head.vue
@@ -1,21 +1,18 @@
 <template>
-  <div
-    v-if="post.author"
-    w:flex="~ row"
-    w:justify="between"
-  >
-    <NuxtLink
-      :to="localePath(`/user/${post.author.username}`)"
-      w:flex="~ row"
-    >
+  <div v-if="post.author" w:flex="~ row" w:justify="between">
+    <NuxtLink :to="localePath(`/user/${post.author.username}`)" w:flex="~ row">
       <UserProfileImage :user="post.author" w:m="r-4" />
       <div w:flex="~ col">
         <span w:text="lg">{{ post.author.username }}</span>
-        <span class="highlight-text" w:text="sm" w:m="t-1">{{ $dayjs.duration($dayjs().diff($dayjs(post.created))).days() > 2 ? $dayjs(post.created).format('DD.MM.YYYY') : $dayjs(post.created).fromNow() }}</span>
+        <span class="highlight-text" w:text="sm" w:m="t-1">{{
+          $dayjs.duration($dayjs().diff($dayjs(post.created))).days() > 2
+            ? $dayjs(post.created).format('DD.MM.YYYY')
+            : $dayjs(post.created).fromNow()
+        }}</span>
       </div>
     </NuxtLink>
     <div @click="showAdditionalOptions = !showAdditionalOptions">
-      <ThreeDots w:cursor="pointer"  />
+      <ThreeDots w:cursor="pointer" />
     </div>
     <transition
       enter-active-class="duration-300 ease-out -bottom-full lg:opacity-0 lg:bottom-auto"
@@ -24,22 +21,37 @@
       leave-class="bottom-0 lg:bottom-auto lg:top-1/2 lg:opacity-100"
       leave-to-class="bottom-full lg:bottom-auto lg:opacity-0"
     >
-      <SlideUp v-if="showAdditionalOptions" :closable="true" @closeSlideUp="showAdditionalOptions = false">
-        <div w:p="6">
-          {{ $t('post.moreActions') }}
-          <ul>
-
-            <li @click="addOrRemoveBookmark(post.id)" v-if="! post.bookmarked">
-              {{ $t('post.actions.addBookmark') }}
+      <SlideUp
+        v-if="showAdditionalOptions"
+        :closable="true"
+        @closeSlideUp="showAdditionalOptions = false"
+      >
+        <div w:p="6" w:mr="12">
+          <h3 w:mb="6">{{ $t('post.moreActions') }}</h3>
+          <ul w:text="base">
+            <li w:my="6">
+              <button
+                v-if="!post.bookmarked"
+                class="block btn-right"
+                @click="addOrRemoveBookmark(post.id)"
+              >
+                {{ $t('post.actions.addBookmark') }}
+              </button>
             </li>
-            <li @click="addOrRemoveBookmark(post.id)" v-if="post.bookmarked">
-              {{ $t('post.actions.removeBookmark') }}
+            <li w:my="6">
+              <button
+                v-if="post.bookmarked"
+                class="block btn-right"
+                @click="addOrRemoveBookmark(post.id)"
+              >
+                {{ $t('post.actions.removeBookmark') }}
+              </button>
             </li>
-
-            <li @click="openPlaylistSelectionModal">
-              {{ $t('post.actions.addToPlaylist') }}
+            <li w:my="6">
+              <button class="block btn-right" @click="openPlaylistSelectionModal">
+                {{ $t('post.actions.addToPlaylist') }}
+              </button>
             </li>
-
             <!--
             <li>Teilen</li>
             <li>Kopieren</li>
@@ -55,7 +67,8 @@
         <PlaylistSelection
           @closeModal="modalOpen = false"
           @createPlaylist="addPostToNewPlaylist"
-          @selectPlaylist="addPostToPlaylist" />
+          @selectPlaylist="addPostToPlaylist"
+        />
       </div>
     </Modal>
   </div>
@@ -65,50 +78,50 @@ import { defineComponent, ref } from '@nuxtjs/composition-api'
 import { postApi } from '@/api/post'
 
 export default defineComponent({
-    props: {
-      post: {
-        type: Object,
-        required: true
-      }
+  props: {
+    post: {
+      type: Object,
+      required: true,
     },
-    setup(props, context) {
-      const { toggleBookmark, addToPlaylist, createPlaylistWithPost } = postApi()
+  },
+  setup(props, context) {
+    const { toggleBookmark, addToPlaylist, createPlaylistWithPost } = postApi()
 
-      const showAdditionalOptions = ref(false)
-      const modalOpen = ref(false)
-      const modalContent = ref('')
+    const showAdditionalOptions = ref(false)
+    const modalOpen = ref(false)
+    const modalContent = ref('')
 
-      function addOrRemoveBookmark(postId) {
-        toggleBookmark(postId)
-        context.emit('toggle-bookmark-state', postId)
-      }
-
-      function openPlaylistSelectionModal() {
-        modalContent.value = 'playlistSelection'
-        modalOpen.value = true
-      }
-
-      function addPostToPlaylist(playlistId) {
-        addToPlaylist(playlistId, props.post.id)
-        modalOpen.value = false
-        showAdditionalOptions.value = false
-      }
-
-      function addPostToNewPlaylist(playlistName) {
-        createPlaylistWithPost(props.post.id, playlistName)
-        modalOpen.value = false
-        showAdditionalOptions.value = false
-      }
-
-      return {
-        showAdditionalOptions,
-        addOrRemoveBookmark,
-        modalOpen,
-        modalContent,
-        openPlaylistSelectionModal,
-        addPostToPlaylist,
-        addPostToNewPlaylist
-      }
+    function addOrRemoveBookmark(postId) {
+      toggleBookmark(postId)
+      context.emit('toggle-bookmark-state', postId)
     }
+
+    function openPlaylistSelectionModal() {
+      modalContent.value = 'playlistSelection'
+      modalOpen.value = true
+    }
+
+    function addPostToPlaylist(playlistId) {
+      addToPlaylist(playlistId, props.post.id)
+      modalOpen.value = false
+      showAdditionalOptions.value = false
+    }
+
+    function addPostToNewPlaylist(playlistName) {
+      createPlaylistWithPost(props.post.id, playlistName)
+      modalOpen.value = false
+      showAdditionalOptions.value = false
+    }
+
+    return {
+      showAdditionalOptions,
+      addOrRemoveBookmark,
+      modalOpen,
+      modalContent,
+      openPlaylistSelectionModal,
+      addPostToPlaylist,
+      addPostToNewPlaylist,
+    }
+  },
 })
 </script>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,6 @@
 export default {
   head: {
-    title: 'Creative Musuem - Badisches Landesmuseum',
+    title: 'Creative Museum - Badisches Landesmuseum',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
