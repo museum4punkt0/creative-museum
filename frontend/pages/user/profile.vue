e548e62d114447e3009ea66201922877bb1fff65
Marco Luig <marco.luig@jwied.de>
2022-08-05 10:07:33 +0200

    [TASK] adapted user profile

--- frontend/pages/user/profile.vue
+++ frontend/pages/user/profile.vue
@@ -1,39 +1,50 @@
 <template>
   <div>
-    <div w:flex="~ col 1">
+    <div w:grid="~ cols-1 lg:cols-12 lg:gap-4">
+      <div w:grid="col-span-3" w:pr="10">
+        <div w:p="6" w:display="md:hidden" class="page-header">
+          <button class="back-btn" @click.prevent="backButton">
+            {{ $t('user.profile.self.headline', {firstName: user.firstName}) }}
+          </button>
+        </div>
 
+        <img
+          v-if="'profilePicture' in user"
+          :src="'https://backend.creative-museum.ddev.site' + user.profilePicture.contentUrl"
+          w:w="21"
+          w:h="21"
+          w:mb="4"
+          w:rounded="full"
+        />
 
-      <div w:p="6" class="page-header">
-        <button class="back-btn" @click.prevent="backButton">
-          {{ $t('user.profile.self.headline', {firstName: user.firstName}) }}
-        </button>
-      </div>
+        <h1 w:text="2xl">{{ user.firstName }} {{ user.lastName }}</h1>
+        <p class="highlight-text" w:mb="2">@{{ user.username }}</p>
+        <p>
+          {{ user.description }}
+        </p>
 
-      <img
-        v-if="'profilePicture' in user"
-        :src="'https://backend.creative-museum.ddev.site' + user.profilePicture.contentUrl"
-        w:w="32"
-        w:h="32"
-        w:mb="12"
-        w:rounded="full"
-      />
+        <NuxtLink to="/user/update" w:align="md:self-start" w:mt="10" w:mb="12" class="btn-primary btn-outline"> {{ $t('user.editProfile') }}</NuxtLink>
 
-      <h1 w:text="2xl">{{ user.firstName }} {{ user.lastName }}</h1>
-      <p class="highlight-text" w:mb="2">@{{ user.username }}</p>
-      <p>
-        {{ user.description }}
-      </p>
+        <h2 w:text="2xl">{{ $t('user.profile.self.points') }}</h2>
 
-      <NuxtLink to="/user/update" w:align="md:self-start" w:mt="10" class="btn-primary btn-outline"> {{ $t('user.editProfile') }}</NuxtLink>
-
-      <h4>{{ $t('user.profile.self.points') }}</h4>
-
-      <div v-for="membership in user.memberships">
-        <div>
-          <span>{{ membership.campaign.title }}</span><br>
-          <span>{{ membership.score }} {{ $t('user.profile.self.score') }}</span>
+        <div v-for="membership in user.memberships" w:align="self-stretch md:self-start" w:mt="3" w:mb="3">
+          <div w:text="sm" w:mb="2" class="highlight-text">{{ membership.campaign.title }}</div>
+          <div
+            w:px="4"
+            w:py="2"
+            w:rounded="full"
+            w:flex="~ row"
+            w:align="items-end"
+            w:justify="center"
+            class="box-shadow"
+          >
+            <span w:text="2xl" w:mr="2">{{ membership.score }}</span>
+            <span>{{ $t('user.profile.self.score') }}</span>
+          </div>
         </div>
       </div>
+
+
 
     </div>
   </div>
