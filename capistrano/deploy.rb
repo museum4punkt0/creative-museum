a591d16d57c9a4b50e751bec5a78f1d5589cb777
Sebastian Verweyen <subjacked@live.de>
2022-08-10 14:45:03 +0200

    [TASK][WIP] Deployment

--- capistrano/deploy.rb
+++ capistrano/deploy.rb
@@ -2,10 +2,8 @@ set :application, 'creative-museum'
 set :repo_url, 'git@bitbucket.org:jwied/creative-museum.git'
 set :keep_releases, 3
 set :deploy_via, :remote_cache
-set :linked_dirs, fetch(:linked_dirs, []).push('typo3/public/fileadmin', 'typo3/public/typo3temp', 'typo3/public/uploads')
-set :linked_files, fetch(:linked_files, []).push('backend/.env', 'typo3/.env')
 set :php_cli, '/opt/plesk/php/8.0/bin/php'
 set :ssh_cli, '/usr/bin/ssh'
 set :log_level, :info
-set :deploy_to, "~/html/#{fetch(:application)}-#{fetch(:stage)}"
+set :deploy_to, "~/#{fetch(:application)}-#{fetch(:stage)}"
 set :composer_install_flags, '--no-dev --no-interaction --no-progress --optimize-autoloader'
--- capistrano/stages/staging.rb
+++ capistrano/stages/staging.rb
@@ -1,9 +1,9 @@
 set :stage, :staging
 
 server 'vpro0803.proserver.punkt.de',
-   user: 'proserver',
+   user: 'proserver'
 
-set :deploy_to, "/var/www/creative-museum-staging/"
+set :deploy_to, '/var/www/creative-museum-staging/'
 set :branch,  'develop'
 set :tmp_dir, '/var/www/creative-museum-staging/tmp'
 set :linked_dirs, fetch(:linked_dirs, []).push('typo3/public/fileadmin', 'typo3/public/typo3temp', 'typo3/public/uploads')
