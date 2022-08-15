dee953217cd63aa19ba945109f571985d5b958ed
Willi Wehmeier <willi.wehmeier@jwied.de>
2022-08-15 14:16:40 +0200

    [TASK] Fiddling around with scripts

--- scripts/frontend-install.sh
+++ scripts/frontend-install.sh
@@ -1,3 +1,7 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 
-cd $1/frontend && npm ci -verbose && npm run build -verbose && mkdir $1/frontend/tmp && touch $1/frontend/tmp/restart.txt && exit 0
+cd $1/frontend || exit
+npm ci
+npm run build
+mkdir $1/frontend/tmp && touch $1/frontend/tmp/restart.txt
+exit 0
