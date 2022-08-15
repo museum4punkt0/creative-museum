62ca815b76595738a1252ddc0b4491aefeaf1e99
Willi Wehmeier <willi.wehmeier@jwied.de>
2022-08-15 08:40:05 +0200

    [TASK] Using verbose output for deployment debugging

--- scripts/frontend-install.sh
+++ scripts/frontend-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd $1/frontend && npm ci && npm run build && mkdir $1/frontend/tmp && touch $1/frontend/tmp/restart.txt && exit 0
+cd $1/frontend && npm ci -verbose && npm run build -verbose && mkdir $1/frontend/tmp && touch $1/frontend/tmp/restart.txt && exit 0
