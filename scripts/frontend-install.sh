fc211b01a87c3478e42061b70c8c1ca5fd24979d
Willi Wehmeier <willi.wehmeier@jwied.de>
2022-08-18 14:26:50 +0200

    [TASK] Debugging stuff

--- scripts/frontend-install.sh
+++ scripts/frontend-install.sh
@@ -1,12 +1,10 @@
 #!/usr/local/bin/bash
 
-truss -f
-
 exec > /tmp/bash.log 2>&1
 set -x
 
 cd $1/frontend || exit
 npm ci || exit
-$1/frontend/node_modules/.bin/nuxt build || exit
+truss -f $1/frontend/node_modules/.bin/nuxt build || exit
 mkdir $1/frontend/tmp && touch $1/frontend/tmp/restart.txt
 exit 0
