03320df05898de97b989eed7147c5a35420f5de3
Willi Wehmeier <willi.wehmeier@jwied.de>
2022-08-18 14:51:33 +0200

    [TASK] Debugging stuff

--- capistrano/stages/staging.rb
+++ capistrano/stages/staging.rb
@@ -1,13 +1,15 @@
 set :stage, :staging
 
 server 'vpro0803.proserver.punkt.de',
-   user: 'vpro0803',
+   user: 'proserver',
    ssh_options: {
-      user: 'vpro0803'
+      user: 'proserver'
    }
 
-set :deploy_to, 'creative-museum-staging/'
+
+
+set :deploy_to, '/var/www/creative-museum-staging/'
 set :branch,  'develop'
-set :tmp_dir, 'creative-museum-staging/tmp'
+set :tmp_dir, '/var/www/creative-museum-staging/tmp'
 set :linked_dirs, fetch(:linked_dirs, []).push('typo3/public/fileadmin', 'typo3/public/typo3temp', 'typo3/public/uploads')
 set :linked_files, ['backend/.env', 'typo3/.env']
