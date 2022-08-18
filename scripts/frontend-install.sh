ed5db803211714b9ce9ba0388fb609ea5c5d14c6
Willi Wehmeier <willi.wehmeier@jwied.de>
2022-08-18 15:46:45 +0200

    [TASK] Try to not install devDependencies

--- scripts/frontend-install.sh
+++ scripts/frontend-install.sh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/bash
 
 cd $1/frontend || exit
-npm ci || exit
-node --max-old-space-size=800 $1/frontend/node_modules/nuxt/bin/nuxt.js build || exit
+npm ci --production || exit
+node $1/frontend/node_modules/nuxt/bin/nuxt.js build || exit
 mkdir $1/frontend/tmp && touch $1/frontend/tmp/restart.txt
 exit 0
