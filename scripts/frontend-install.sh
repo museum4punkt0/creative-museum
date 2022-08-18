9b1cf409a490855862f3ac245d2cfbe3ee6dc543
Willi Wehmeier <willi.wehmeier@jwied.de>
2022-08-18 15:07:04 +0200

    [TASK] Remove ktrace debugging stuff from frontend install script

--- scripts/frontend-install.sh
+++ scripts/frontend-install.sh
@@ -1,10 +1,7 @@
 #!/usr/local/bin/bash
 
-exec > /tmp/bash.log 2>&1
-set -x
-
 cd $1/frontend || exit
 npm ci || exit
-ktrace -i $1/frontend/node_modules/.bin/nuxt build || exit
+$1/frontend/node_modules/.bin/nuxt build || exit
 mkdir $1/frontend/tmp && touch $1/frontend/tmp/restart.txt
 exit 0
