9cf020c4ef6d7f146da228388ee5ccc94f61448c
Willi Wehmeier <willi.wehmeier@jwied.de>
2022-08-18 15:25:21 +0200

    [TASK] Try to set memory limit to nuxt build task

--- scripts/frontend-install.sh
+++ scripts/frontend-install.sh
@@ -2,6 +2,6 @@
 
 cd $1/frontend || exit
 npm ci || exit
-$1/frontend/node_modules/.bin/nuxt build || exit
+node --max-old-space-size=800 $1/frontend/node_modules/nuxt/bin/nuxt.js build || exit
 mkdir $1/frontend/tmp && touch $1/frontend/tmp/restart.txt
 exit 0
