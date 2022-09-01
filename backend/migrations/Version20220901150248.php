175d7b9809bbe72864b74dbb81ece574bc6bfb79
Willi Wehmeier <willi.wehmeier@jwied.de>
2022-09-01 17:05:05 +0200

    [TASK] Add missing migrations for last commit

---
+++ backend/migrations/Version20220901150248.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220901150248 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX active ON campaign');
+        $this->addSql('CREATE INDEX campaign_collection_index ON campaign (active, notified, start, stop)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX campaign_collection_index ON campaign');
+        $this->addSql('CREATE INDEX active ON campaign (active, notified, start, stop)');
+    }
+}
