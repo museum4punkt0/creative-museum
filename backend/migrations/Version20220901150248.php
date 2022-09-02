c34b019e52f0a9e7b58fb15cd51351dc4a412300
nicoborschke <nico.borschke@jwied.de>
2022-09-02 10:43:55 +0200

    [TASK] Drop index if exists to prevent deployment errors

--- backend/migrations/Version20220901150248.php
+++ backend/migrations/Version20220901150248.php
@@ -20,7 +20,7 @@ final class Version20220901150248 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP INDEX active ON campaign');
+        $this->addSql('DROP INDEX IF EXISTS active ON campaign');
         $this->addSql('CREATE INDEX campaign_collection_index ON campaign (active, notified, start, stop)');
     }
 
@@ -28,6 +28,6 @@ final class Version20220901150248 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP INDEX campaign_collection_index ON campaign');
-        $this->addSql('CREATE INDEX active ON campaign (active, notified, start, stop)');
+        $this->addSql('CREATE INDEX IF NOT EXISTS active ON campaign (active, notified, start, stop)');
     }
 }
