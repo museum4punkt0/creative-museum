32a1b3620339807addfeaa6bb5e3ab3b172d997e
Sebastian Verweyen <subjacked@live.de>
2022-09-02 10:29:47 +0200

    [TASK] Colorchange

--- frontend/middleware/user.js
+++ frontend/middleware/user.js
@@ -1,5 +1,16 @@
 import { defineNuxtMiddleware } from '@nuxtjs/composition-api'
 export default defineNuxtMiddleware((ctx) => {
+
+  if (process.client) {
+    document.documentElement.style.removeProperty(
+      '--highlight'
+    )
+
+    document.documentElement.style.removeProperty(
+      '--highlight-contrast'
+    )
+  }
+
   if (process.client && ctx.$auth.loggedIn) {
     ctx.$auth.fetchUser()
   }
