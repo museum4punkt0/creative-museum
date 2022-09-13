2c530c09c72e7a8f69eb10f775de5c1c5d153d25
Sebastian Verweyen <subjacked@live.de>
2022-09-13 14:57:52 +0200

    [TASK] New posts available

--- frontend/pages/campaigns/_id.vue
+++ frontend/pages/campaigns/_id.vue
@@ -9,6 +9,7 @@
       <div class="lg:col-span-6">
         <div v-if="campaign">
           <CampaignHead v-if="campaign" :campaign="campaign" />
+          <button v-if="newPostsAvailable" class="btn-outline mx-auto px-6" @click.prevent="showNewPosts">{{ $t('campaign.newPosts') }}</button>
           <PostList v-if="posts && posts.length" :posts="posts" source="campaign" />
           <UtilitiesLoadingIndicator v-else-if="!posts" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
           <div v-else><button class="btn-highlight w-full mt-10" @click.prevent="showAddModal">{{ $t('post.new') }}</button></div>
@@ -56,6 +57,8 @@ export default defineComponent({
     const { $breakpoints } = useContext()
     const store = useStore()
     const posts = ref(null)
+    const newPosts = ref(null)
+    const newPostsAvailable = ref(false)
     const campaign = ref(null)
 
     const newPost = computed(() => store.state.newPostOnCampaign)
@@ -105,12 +108,37 @@ export default defineComponent({
       }
     }
 
+    async function refetchPosts() {
+      newPosts.value = await fetchPostsByCampaign(
+        route.value.params.id,
+        store.state.currentSorting,
+        store.state.currentSortingDirection,
+        1
+      )
+
+      if (newPosts.value[0].id !== posts.value[0].id) {
+        newPostsAvailable.value = true
+      }
+    }
+
+    function showNewPosts() {
+      if (newPosts && newPosts.value.length) {
+        posts.value = newPosts.value
+        newPostsAvailable.value = false
+      }
+    }
+
     function showAddModal() {
       store.dispatch('showAddModal')
     }
 
     onMounted(async () => {
       await loadCampaign()
+
+      setInterval(() => {
+        refetchPosts()
+      }, 2500)
+
     })
 
     return {
@@ -118,7 +146,10 @@ export default defineComponent({
       posts,
       newPost,
       isLargerThanLg,
-      showAddModal
+      newPostsAvailable,
+      showAddModal,
+      refetchPosts,
+      showNewPosts
     }
   },
 })
