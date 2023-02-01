9479c9d7a40cb57de35f54e1b1b9013b0592610d
Sebastian Verweyen <subjacked@live.de>
2023-02-01 15:56:40 +0100

    [TASK] Updated frontend packages

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,7 +2,6 @@ export default {
   head: {
     title: 'Creative Museum - Badisches Landesmuseum',
     meta: [
-      { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
