4916f80c4276e96d762f9355fe1fccee8993188c
Sebastian Verweyen <subjacked@live.de>
2023-02-15 13:38:29 +0100

    [FIX] Typo

--- capistrano/stages/staging.rb
+++ capistrano/stages/staging.rb
@@ -11,4 +11,4 @@ server 'vpro0803.proserver.punkt.de',
 set :php_cli, '/usr/local/bin/php'
 set :ssh_cli, '/usr/bin/ssh'
 set :tmp_dir, '/var/www/creative-museum-staging/tmp'
-set :backend_url, 'https://testbackend.creativemuseum.landemuseum.de'
+set :backend_url, 'https://testbackend.creativemuseum.landesmuseum.de'
