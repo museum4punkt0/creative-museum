8c4220bb967831bcd8df409890072f2cd5dbbe0c
Sebastian Verweyen <subjacked@live.de>
2023-02-27 15:00:28 +0100

    [TASK] Kiosk view

--- frontend/components/kiosk/Filter.vue
+++ frontend/components/kiosk/Filter.vue
@@ -1,32 +1,26 @@
 <template>
   <div class="relative">
-      <button
-        class="btn-outline border-border-1 border-white text-lg block w-full text-center py-3 self-start rounded-full mb-3 py-1 px-2"
-        :class="currentSorting === 'date' ? 'active' : ''"
+      <div
+        class="border-1 text-lg block w-full text-center py-3 self-start rounded-full mb-3 py-1 px-2"
+        :class="currentSorting === 'date' ? 'border-$highlight bg-$highlight' : 'border-white'"
         :aria-current="currentSorting === 'date' ? 'true' : 'false'"
-        type="button"
-        @click.prevent="resetFilter"
       >
         {{ $t('filter.newest') }}
-      </button>
-      <button
-        class="btn-outline border-border-1 border-white text-lg block w-full text-center py-3 self-start rounded-full mb-3 ml-3 lg:ml-0 py-1 px-2"
-        :class="currentSorting === 'votestotal' ? 'active' : ''"
+      </div>
+      <div
+        class="border-1  text-lg block w-full text-center py-3 self-start rounded-full mb-3 ml-3 lg:ml-0 py-1 px-2"
+        :class="currentSorting === 'votestotal' ? 'border-$highlight bg-$highlight' : 'border-white'"
         :aria-current="currentSorting === 'votestotal' ? 'true' : 'false'"
-        type="button"
-        @click.prevent="toggleRelevanceFilter"
       >
         {{ $t('filter.relevant') }}
-      </button>
-      <button
-        class="btn-outline border-border-1 border-white text-lg block w-full text-center py-3 self-start rounded-full mb-3 ml-3 lg:ml-0 py-1 px-2"
-        :class="currentSorting === 'controversial' ? 'active' : ''"
+      </div>
+      <div
+        class="border-1 text-lg block w-full text-center py-3 self-start rounded-full mb-3 ml-3 lg:ml-0 py-1 px-2"
+        :class="currentSorting === 'controversial' ? 'border-$highlight bg-$highlight' : 'border-white'"
         aria-haspopup="true"
-        type="button"
-        @click.prevent="toggleControversialFilter"
       >
         {{ $t('filter.controversial') }}
-      </button>
+      </div>
   </div>
 </template>
 <script>
